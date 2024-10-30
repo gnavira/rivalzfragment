9bbe8460cfd96cc815dd6ad9073691b2de798a98
Navira Gayatri <141285340+gnavira@users.noreply.github.com>
2024-10-30 20:33:45 +0000

    Upload File new file:   abi/abi.js new file:   chains/index.js new file:   chains/mainnet/ethereum.js new file:   chains/testnet/rivalzTestnet.js new file:   chains/utils/chainTemplate.js new file:   chains/utils/utils.js new file:   index.js new file:   log.txt new file:   package.json new file:   privateKeys.json new file:   yml.txt

---
+++ index.js
@@ -0,0 +1,61 @@
+const { ethers } = require('ethers');
+const chains = require('./chains');
+const provider = chains.testnet.rivalzTestnet.provider();
+const explorer = chains.testnet.rivalzTestnet.explorer;
+const fs = require('fs');
+const moment = require('moment-timezone');
+const { displayHeader, delay } = require('./chains/utils/utils');
+const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
+const { RIVALZ_ABI } = require('./abi/abi');
+const CLAIM_CA = '0xF0a66d18b46D4D5dd9947914ab3B2DDbdC19C2C0';
+
+function appendLog(message) {
+  fs.appendFileSync('log.txt', message + '\n');
+}
+
+async function doClaim(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  try {
+    const claimContract = new ethers.Contract(
+      CLAIM_CA,
+      RIVALZ_ABI,
+      wallet
+    );
+    const txClaim = await claimContract.claim()
+    const receipt = await txClaim.wait(10);
+    const successMessage = `Transaction Confirmed in block ${receipt.blockNumber}`;
+    console.log(successMessage.blue);
+    appendLog(successMessage);
+    return txClaim.hash;
+
+  } catch (error) {
+    const errorMessage = `[$timezone] Error executing transaction: ${error.message}`;
+    console.log(errorMessage.red);
+    appendLog(errorMessage);
+  }
+}
+
+async function runClaim() {
+  displayHeader();
+  const timezone = moment().tz('Asia/Jakarta').format('HH:mm:ss [WIB] DD-MM-YYYY');
+  for (const PRIVATE_KEY of PRIVATE_KEYS) {
+    try {
+	 for (let i = 0; i < 20; i++) {
+      await delay(15000);
+      const receiptTx = await doClaim(PRIVATE_KEY);
+      if (receiptTx) {
+        const successMessage = `[${timezone}] Transaction Hash: ${explorer.tx(receiptTx)}`;
+        console.log(successMessage.cyan);
+        appendLog(successMessage);
+      }
+      console.log('');
+	}
+    } catch (error) {
+      const errorMessage = `[${timezone}] Error processing transaction. Please try again later.`;
+      console.log(errorMessage.red);
+      appendLog(errorMessage);
+      console.log('');
+	}
+  }
+}
+runClaim();
